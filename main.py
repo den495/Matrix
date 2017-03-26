b4fd0d2102a5908dd129bcd2c19bb3d41f94019e
den495 <8465800@mail.ru>
2017-03-26 11:10:34 +0300

    update in def main {print(getState)}

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 class State:
-   def __init__(self, name, state):
-      self.name = name
-      self.state = state
-   def getName(self):
-      return self.name
-   def getsState(self):
-      return self.state
+  def __init__(self, name, state):
+    self.name = name
+    self.state = state
+  def getName(self):
+    return self.name
+  def getState(self):
+    return self.state
 
 
 def main():
-   machine = State("Machine", 1)
-   print(machine.getName())
+  machine = State("Machine", 1)
+  print(machine.getState())
 
 
 if __name__ == "__main__":
-   main()
+  main()
